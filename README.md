7dc38aaca2e27f24a079a1690ca4377e1c81d692
Oliver Wigren <57063539+oliverwigren@users.noreply.github.com>
2023-12-16 11:16:06 +0100

    files from CS50 vscode

--- README.md
+++ README.md
@@ -1,39 +1,60 @@
 # Password Manager
 
-This is my final project to HarvardX's CS50 course on the foundations of computer science. I have choosen to create a simple password manager.
-<!--
+This is my final project to HarvardX's CS50 course on the foundations of computer science. I have choosen to create a simple password manager. Its features are listed below.
+
 ### Video
-Link to my video: http
--->
-## Features:
+[Link to my video](https://youtu.be/yY4USK412Ys)
 
-< :warning: DISCLAIMER
 
-- Generate passwords by three difficulties.
+## :warning: DISCLAIMER: CREATED PASSWORDS ARE NOT ENCRYPTED AND ARE STORED AS PLAIN TEXT. NEVER USE YOUR OWN REAL PASSWORD OR CREDENTIALS!!
+
+## Features
+
+<!--- Generate passwords by three difficulties.-->
 - View all passwords in a table
-- Delete, Change and, Save passwords to a database.
+- Create, Change and Save passwords to a database.
 - Log-in with password and username
 - Register as a new user with name, username and password
 - Stay logged in with sessions
+- Log-in-password is hashed
+<!-- - Entered passwords are encrypted--->
 
-## Powered by:
+## Technologies used
 
 - Python
-- SQL
+- SQLite3
 - HTML
+- CSS
+- Bootstrap
 - Flask
 - Jinja
+- werkzeug.security
+
+## Usage and installation instructions
+This project is in very early development and is not meant to be installed or used. In this stage is the point to show the source code.
 
 ## Deep dive
 
 ### app.py
-This the main functionality to the manager.
+This the main functionality to the manager. This is where everything is powered. Every function in more detail below.
 
-### index.html
+### Log-in
+The user enters a password and a username. The app checks for invalid input and if invalid input is detected, then a error message is returned and displayed. Otherwise, the app continues and checks the users-database for an account with that username and password. If more then one account or none is found then once again a error message is displayed. If everything is correct then a session is started with the unique user-id and the user is redirected to the first-page (index.html).
+### Log-out
+The session is set None and the user is redirected to the login-page.
+### Register
+The register function takes a username, password and a password confirmation as input. The app checks input and returns an error message if the input is incorrect. The app then checks with the database if the username is unique. If the username is not unique then an error message is displayed to the user. If the username is unique then the user credentials are inserted into the users-database. (The password is hashed with werkzeug.security to make it secure to store passwords in a database). Then the app gets the user-id to start a session and the user is afterwards redirected to first-page (index.html).
+### Create
+The create function takes a website name, a username and a password and makes sure that everything has been inputed correctly. If so the app checks if there already exists an passwords saved to database with the same website name. If either of this fails then a error is sent back and a text is shown to the user what went wrong. On the other hand, if everything went fine then the credentials are inserted into the passwords database and the user is redirected to the first-page. There is also an option to click on the back button, if pressed will the user be redirected to the first-page and the credentials will not be inserted into the database.
+### Index
+The app searches for any passwords in the specific user's part of the password-database, if none are found will nothing be shown. Otherwise, the passwords, websites, and usernames be shown in a table. In each row is there a button to change password and/ or username. If clicked will the "change" function be called. (Described below.) There is also a additional button: "New Password", if clicked will the "create" function be called. (Described above)
+### Change
+The change function takes a website name, username and password. The website name is set not by user through text input but depending on which password that the "change" button is pressed. The function checks that correct input has been submitted and if not will an error be sent back. If the input is correct then the app will check that there is a (only one) password that corresponds with that user id and that website name. If not will an error be sent back to the user. Otherwise, will the username and password be updated in the database. Afterwards will the user be redirected to the first-page.
 
-
-
-
+<!--Using:
+werkzeug.security
+cryptography.fernet
+-->
 
 
 © 2023 Oliver Wigren
--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-#antal = input("Hur många tecken?")
-#svårighet = input("1-5, hur svårt")
-
-#easy[] = {a,b,c,d,e,f,g}
-#medium[] = {'1','2','3','4','5'}
-#hard[] = {'@','!','#','€','$','%'}
-
 from cs50 import SQL #Ta bort
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
+from functools import wraps
+import rsa
+from werkzeug.security import check_password_hash, generate_password_hash
+from cryptography.fernet import Fernet
 
 # Configure application
 app = Flask(__name__)
@@ -18,54 +15,214 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///passwords.db") #Ändra
 
-passwords = {}
+passwords = {} #Ta bort?
 
 # Configure session
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-INVALIDCREDENTIALS = "Wrong password or username"
+#publicKey, privateKey = rsa.newkeys(2048)
+#key = Fernet.generate_key()
+#fernet = Fernet(key)
+
+# Define login_required
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route("/")
+@login_required
 def index():
-    name = "Oliver" # db
-    if not session.get("name"):
-        return redirect("/login")
-    #passwords = db.execute("SELECT address, username, password FROM password JOIN user ON user.id = password.id WHERE id = id")
-    return render_template("index.html", name=name)
+    # Get username
+    name = db.execute("SELECT username FROM users WHERE id = ?",
+    session["user_id"])[0]['username']
+    # Get passwords
+    passwords = db.execute("SELECT website, username, password FROM passwords WHERE id = ?", session["user_id"])
+    if len(passwords) == 0:
+        return render_template("index.html")
+    # List of decrypted passwords
+    #decrypted = passwords[3]['password']
+    #mss = passwords[0]['password']
+    #decrypted = str(db.execute("SELECT website, username, password FROM passwords WHERE id = ?", session["user_id"])[3]['password'])
+    #encrypted = rsa.encrypt(mss.encode(), publicKey)
+    #passwords[3]['password']
+    #decrypted = fernet.decrypt(mss).decode()
+    #rsa.decrypt(mss, privateKey).decode()
+    #for pwd in passwords:
+        #decrypted.append(rsa.decrypt(pwd['password'], privateKey).decode())
+
+    return render_template("index.html", username=name, passwords=passwords)#, decrypted=decrypted) #TODO: Handle Decryption
+
+@app.route("/create", methods = ["GET", "POST"])
+@login_required
+def create():
+    if request.method == "GET":
+        return render_template("create.html")
+    else:
+        # If website was not submitted
+        if not request.form.get("website"):
+            return render_template("create.html", errorText = "Need to enter website")
+        # If username not submitted
+        if not request.form.get("username"):
+            return render_template("create.html", errorText = "Need to enter username")
+        # If username not submitted
+        elif not request.form.get("password"):
+            return render_template("create.html", errorText = "Need to enter password")
+
+        #TODO: Check if website already exist
+        if 0 != len(db.execute("SELECT * FROM passwords WHERE website = ? AND id = ?", request.form.get("website"), session["user_id"])):
+            return render_template("create.html", errorText = "Password for this website already registered")
+        #TODO: Time
+
+        # Insert new password to
+        db.execute("INSERT INTO passwords (id, username, website, password)VALUES (?, ?, ?, ?)",
+            session["user_id"],
+            request.form.get("username"),
+            request.form.get("website"),
+            #generate_password_hash(request.form.get("password"))
+            request.form.get("password")
+            #rsa.encrypt(request.form.get("password").encode(), publicKey)
+            #fernet.encrypt(request.form.get("password").encode())
+            )
+
+        return redirect("/")
+
+@app.route("/change", methods = ["GET", "POST"])
+@login_required
+def change():
+    if request.method == "GET":
+        return redirect("/")
+    else:
+        # Check if value is correct
+        if len(db.execute("SELECT * FROM passwords WHERE website = ? AND id = ?", request.form["change_button"], session["user_id"])) != 1:
+            return redirect("/")
+
+        return render_template("change.html", website=request.form["change_button"])
+
+@app.route("/changed", methods = ["GET", "POST"])
+@login_required
+def changed():
+    if request.method == "GET":
+        return redirect("/")
+    else:
+        # If website was not submitted
+        if not request.form.get("website"):
+            return render_template("create.html", errorText = "Need to enter website")
+        # If username not submitted
+        if not request.form.get("username"):
+            return render_template("create.html", errorText = "Need to enter username")
+        # If username not submitted
+        elif not request.form.get("password"):
+            return render_template("create.html", errorText = "Need to enter password")
+
+        # Make sure it exists
+        if len(db.execute("SELECT * FROM passwords WHERE website = ? AND id = ?", request.form.get("website"), session["user_id"])) != 1:
+            return render_template("create.html", errorText = "Incorrect") #TODO: Handle correct
+
+        # Change
+        db.execute("UPDATE passwords SET username = ?, password = ? WHERE website = ? AND id = ?",
+                   request.form.get("username"),
+                   request.form.get("password"), #TODO: Encrypt
+                   #rsa.encrypt(request.form.get("password").encode(), publicKey),
+                   #fernet.encrypt(request.form.get("password").encode()),
+                   request.form.get("website"),
+                   session['user_id'])
+        return redirect("/")
+
+#@app.route("/decrypt", methods = ["GET", "POST"])
+#@login_required
+#def decrypt():
+    #if request.method == "GET":
+        #return redirect("/")
+    #else:
+        #return render_template("index.html",)
 
 
 @app.route("/login", methods = ["GET", "POST"])
 def login():
-    if session.get("name"):
+    session.clear()
+
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        # If username not submitted
+        if not request.form.get("username"):
+            return render_template("login.html", errorText = "Need to enter username")
+        # If username not submitted
+        elif not request.form.get("password"):
+            return render_template("login.html", errorText = "Need to enter password")
+        # Get user details
+        users = db.execute(
+            "SELECT * FROM users WHERE username = ?",
+            request.form.get("username")
+        )
+        # Check if user exists and password is correct
+        if len(users) != 1 or not check_password_hash(users[0]["password"], request.form.get("password")):
+            return render_template("login.html", errorText="invalid username and/or password")
+
+        # Start session for user
+        session["user_id"] = users[0]["id"]
+
         return redirect("/")
-    if request.method == "POST":
-        if request.form.get("password") == "123": #db.execute("SELECT password"):
-            session["name"] = request.form.get("username") + request.form.get("password")
-            return redirect("/")
-        else:
-            return render_template("login.html", errorText = INVALIDCREDENTIALS)
-    return render_template("login.html")
+
 
 @app.route("/logout")
 def logout():
-    session["name"] = None
+    session["user_id"] = None
     return redirect("/login")
 
-@app.route("/register", methods = ["GET", "POST"])
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "POST":
-        if request.form.get("password") == request.form.get("password2"):
-            name = request.form.get("name")
-            username = request.form.get("username")
-            password = request.form.get("password")
-            db.execute("INSERT INTO user (name, username, password) VALUES (?, ?, ?)", name, username, password)
-            session["name"] = request.form.get("username") + request.form.get("password")
-            return redirect("/")
-        else:
-            return render_template("register.html", errorText = INVALIDCREDENTIALS)
-    elif request.method == "GET":
+    if request.method == "GET":
         return render_template("register.html")
-    return render_template("index.html")
+    else:
+        # If not username is submitted
+        if not request.form.get("username"):
+            return render_template("register.html", errorText = "Need to enter username")
+
+        # If not password was submitted
+        elif not request.form.get("password"):
+            return render_template("register.html", errorText = "Need to enter password")
+
+        # If password was not submitted
+        elif not request.form.get("password2"):
+            return render_template("register.html", errorText = "Need to enter password confirmation")
+
+        # Ensure the password and confirmation is the same
+        elif request.form.get("password") != request.form.get("password2"):
+            return render_template("register.html", errorText = "The passwords are not identical")
+
+        # Ensure the username is unique
+        elif (
+            len(db.execute("SELECT * FROM users WHERE username = ?",
+                    request.form.get("username"),)) > 0 ):
+            return render_template("register.html", errorText = "Username already taken, choose another")
+
+        # Save user to the database
+        db.execute("INSERT INTO users (username, password)VALUES (?, ?)",
+            request.form.get("username"),
+            #hash
+            #argon2.hash_password(bytes(request.form.get("password"),"utf-8")),)
+            generate_password_hash(request.form.get("password")))
+
+        # Get the users credentials
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
