4e4134758c2af8e3dc682fb8224867ae04cff5f1
Oliver Wigren <57063539+oliverwigren@users.noreply.github.com>
2024-05-17 10:46:28 +0200

    clean up

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from cs50 import SQL # To be removed
+from cs50 import SQL #TODO: Remove, use other SQL tool
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from functools import wraps
